a965fff27a946fb94bf32bd35a17b844a09f5601
earth-emoji <reginaldchapple@outlook.com>
2020-05-27 22:27:19 -0700

    fixed form

--- src/events/views/events.py
+++ src/events/views/events.py
@@ -24,21 +24,21 @@ class EventListing(ListAPIView):
         # filter the queryset based on the filters applied
         campaign = Campaign.objects.get(slug=self.request.query_params.get('campaign', None))
 
-        query_list = Event.objects.filter(campaign=campaign)
+        query_list = Event.objects.filter(campaign=campaign).order_by("-start_time")
 
         visibility = self.request.query_params.get('visibility', None)
         event_day = self.request.query_params.get('day', None)
         sort_by = self.request.query_params.get('sort_by', None)
 
         if visibility:
-            query_list = query_list.filter(visibility=visibility)
+            query_list = query_list.filter(visibility=visibility).order_by("name")
         if event_day:
-            query_list = query_list.filter(start_time__day=event_day)
+            query_list = query_list.filter(start_time__day=event_day).order_by("-start_time")
 
         if sort_by == "name":
             query_list = query_list.order_by("name")
         elif sort_by == "start_time":
-            query_list = query_list.order_by("start_time")
+            query_list = query_list.order_by("-start_time")
         return query_list
 
 def get_visibility(request, slug):
